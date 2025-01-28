5d1de552860f9a21ddf986637ca369ea245328c5
binting1 <bintingchenn123@gmail.com>
2025-01-28 14:04:09 -0500

    123

--- app/src/App.vue
+++ app/src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Business Name"/>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -23,7 +23,8 @@ import HelloWorld from './components/HelloWorld.vue'
 <style scoped>
 header {
   line-height: 1.5;
-  max-height: 100vh;
+  max-height: 20vh;
+
 }
 
 .logo {
