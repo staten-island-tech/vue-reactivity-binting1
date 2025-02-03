75b7fd564e2c5de6bba6489dde0c385132578079
binting1 <bintingchenn123@gmail.com>
2025-02-03 14:01:46 -0500

    123

--- app/src/components/FloorPlan.vue
+++ app/src/components/FloorPlan.vue
@@ -1,19 +1,18 @@
 <template>
-    <div class="">
-        <h2 v-for="table in tables" :key="table">{{ table.id }}</h2>
-        <h3 v-for="table in tables" :key="table">{{ table.status }}</h3>
-        <li v-for="table in tables">
-            
-        </li>
-    </div>
+    <div class="container">
+        <tableCard v-for="table in tables" :key="table.id" :table/>
+    </div> 
 </template>
 
 <script setup>
+import tableCard from "../components/tableCard.vue"
 const tables = [
-    {id:"1", status:"occupied"}
+    {id:"1", status:"occupied"},
+    {id:"2", status:"unoccupied"},
 ]
 
 </script>
 
 <style scoped>
+
 </style>
---
+++ app/src/components/tableCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <h2>{{ table.id }}</h2>
+        <h2>{{ table.status }}</h2>
+        <button >register for spot</button>
+    </div>
+</template>
+
+<script setup>
+defineProps ({
+    table: Object,
+});
+</script>
+
+<style scoped>
+.container {
+    border: 2px solid black;
+    padding: 2px;
+    margin: 10px;
+}
+</style>
