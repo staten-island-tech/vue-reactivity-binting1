b0d551d19436b39ff9e0ec74cbf09836a0f18c0c
binting1 <bintingchenn123@gmail.com>
2025-02-05 14:00:39 -0500

    123

--- app/src/App.vue
+++ app/src/App.vue
@@ -8,15 +8,15 @@ import HelloWorld from './components/HelloWorld.vue'
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
     <div class="wrapper">
         <HelloWorld msg="Business Name"/>
-    <!-- <nav>
-      <RouterLink to="/">Reservations</RouterLink>
-    </nav> -->
-
-    
     </div>
-
   </header>
-  <!-- <RouterView/> -->
+  
+  <nav>
+      <RouterLink to="/">Reservations</RouterLink>
+  </nav>
+
+  <RouterView/>
+
 </template>
 
 <style scoped>
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -9,6 +9,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/about',
+      name: 'About',
+      component: () => import('../views/About.vue')
+    },
     
   ],
 })
